6cc67e7b2f258ea1846eac0d324fa2956d8a43da
Christopher <thorstedc266935@gmail.com>
2017-07-25 15:00:24 -0500

    miniTime

---
+++ js/timeDirective.js
@@ -0,0 +1,10 @@
+angular.module("timeApp").directive("showTime", function() {
+return {
+    restrict: 'E',
+    template: '<h2> The current time is {{time}} </h2>',
+    link: function(scope, element, attrs){
+      var currentTime = new Date();
+      scope.time = currentTime.toString();
+}
+
+}});
